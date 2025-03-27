c4c7f93ec8c7c431154f6941cad69073b5a169a5
Andresleetter <andresleetter@gmail.com>
2025-03-26 23:32:05 -0300

    complete class exercises

--- Basic/23-class-exercises.js
+++ Basic/23-class-exercises.js
@@ -15,7 +15,7 @@ class CarMethod {
     this.color = color
   }
   starts () {
-    console.log('Ther car starts')
+    console.log('The car starts')
   }
 }
 
@@ -66,8 +66,87 @@ smallCar.starts()
 
 // 7. Crea una clase que haga uso de getters y setters
 
+class GetCar {
+
+  #brand
+  #plate
+
+  constructor (brand, color, plate) {
+    this.#brand = brand
+    this.color = color
+    this.#plate = plate
+  }
+  get brand () {
+    return this.#brand
+  }
+}
+
 // 8. Modifica la clase con getters y setters para que use propiedades privadas
+
+class GetSetCar {
+
+  #brand
+  #plate
+
+  constructor (brand, color, plate) {
+    this.#brand = brand
+    this.color = color
+    this.#plate = plate
+  }
+  get brand () {
+    return this.#brand
+  }
+  get plate () {
+    return this.#plate
+  }
+  set plate (newPlate) {
+    if (typeof newPlate === 'string' && newPlate.length > 0) {
+      this.#plate = newPlate;
+    } else {
+      console.error('Invalid plate value')
+    }
+  } 
+}
 
 // 9. Utiliza los get y set y muestra sus valores
 
+let car1 = new GetSetCar ('Toyota', 'Black', 'BOSS1234')
+
+console.log(car1.plate)
+console.log(car1.brand)
+car1.plate = 'VESS1234'
+console.log(car1.plate)
+
 // 10. Sobrescribe un método de una clase que utilice herencia 
+
+class NewCar {
+  constructor (brand, color) {
+    this.brand = brand
+    this.color = color
+  } 
+  starts () {
+    console.log('The car starts')
+  }
+}
+
+class ElectricCar extends NewCar {
+  constructor(brand, color, battery) {
+  super(brand, color)
+  this.battery = battery
+  } 
+  starts () {
+    super.starts ()
+    console.log('The electric car starts')
+  }
+  checkBattery() {
+    console.log('The battery level is at 100%')
+  }  
+}
+
+let regularCar = new NewCar('Toyota', 'Black')
+regularCar.starts()
+
+let electricCar = new ElectricCar('Tesla', 'White', '100 kWh')
+electricCar.starts()
+
+electricCar.checkBattery()
