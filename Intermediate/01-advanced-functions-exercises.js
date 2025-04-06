6381455de91d04eda7ed1f43e91ccdad474ba106
Andresleetter <andresleetter@gmail.com>
2025-04-05 23:50:14 -0300

    complete advanced functions exercises

--- Intermediate/01-advanced-functions-exercises.js
+++ Intermediate/01-advanced-functions-exercises.js
@@ -51,14 +51,99 @@ console.log(potencia(5, 5))
 
 // 4. Crea una función createCounter() que reciba un valor inicial y retorne un objeto con métodos para increment(), decrement() y getValue(), utilizando un closure para mantener el estado
 
+function createCounter (initialValue = 0) {
+  let counter = initialValue
+
+  return {
+    increment () {
+    counter++
+    console.log(`Contador: ${counter}`)
+  },
+    decrement () {
+      counter--
+      console.log(`Contador: ${counter}`)
+    },
+    getValue () {
+      return counter
+    }
+  } 
+}
+
+const counter = createCounter(5)
+counter.increment()
+counter.decrement()
+console.log(counter.getValue())
+
 // 5. Crea una función sumManyTimes(multiplier, ...numbers) que primero sume todos los números (usando parámetros Rest) y luego multiplique el resultado por multiplier
+
+function sumManyTimes (multiplier, ...numbers) {
+  let result = 0
+
+  for (let number of numbers) {
+    result += number
+  }
+  return result * multiplier
+}
+
+console.log(sumManyTimes(2, 1, 2, 3, 4, 5))
 
 // 6. Crea un Callback que se invoque con el resultado de la suma de todos los números que se le pasan a una función
 
+function sum (...numbers) {
+  let result = 0
+
+  for (let number of numbers) {
+    result += number
+  }
+  return result
+}
+
+function processSum (numbers, callback) {
+  const result = sum (...numbers)
+
+  callback(result)
+}
+function processResult (result) {
+  console.log(result)
+}
+
+processSum([2, 2, 2, 2], processResult)
+
 // 7. Desarrolla una función parcial
+
+function partialMult (a) {
+  return function (b, c) {
+    return mult(a, b, c)
+  }
+}
+
+const multWith = partialMult(4)
+
+console.log(multWith(2, 3))
 
 // 8. Implementa un ejemplo que haga uso de Spread
 
+const numbers = [1, 2, 3, 3, 4]
+
+function sumSpread (a, b, c, d, e) {
+  return a + b + c + d + e
+}
+
+console.log(sumSpread(...numbers))
+
 // 9. Implementa un retorno implícito
 
+const sumImplicit = (a, b) => a + b
+console.log(sumImplicit(2, 5))
+
 // 10. Haz uso del this léxico
+
+const handler = {
+  name: 'Andres',
+  age: 23,
+  greeting: function() {
+    console.log(`Hola, ${this.name} de ${this.age} anhos`)
+  }
+}
+
+handler.greeting()
