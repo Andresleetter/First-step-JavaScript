45a76f7424bb07ea98745a3afbe9f0ea4f85574e
Andresleetter <andresleetter@gmail.com>
2025-04-26 23:10:36 -0300

    .

--- Intermediate/07-async.js
+++ Intermediate/07-async.js
@@ -23,3 +23,44 @@ console.log("Fin")
 // 5. El proceso se repite
 
 // Código asíncrono
+
+// Callbacks
+
+console.log("Inicio")
+
+setTimeout(() => {
+    console.log("Esto se ejecuta después de 2 segundos")
+}, 2000)
+
+console.log("Fin")
+
+// Problema: Callback Hell
+
+function step1(callback) {
+    setTimeout(() => {
+        console.log("Paso 1 completado")
+        callback()
+    }, 1000)
+}
+
+function step2(callback) {
+    setTimeout(() => {
+        console.log("Paso 2 completado")
+        callback()
+    }, 1000)
+}
+
+function step3(callback) {
+    setTimeout(() => {
+        console.log("Paso 3 completado")
+        callback()
+    }, 1000)
+}
+
+step1(() => {
+    step2(() => {
+        step3(() => {
+            console.log("Todos los pasos completados")
+        })
+    })
+})
